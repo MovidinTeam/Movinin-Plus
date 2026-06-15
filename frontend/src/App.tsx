6bfa3db3df908527e233b159f32dfe144b1e35f4
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2026-06-15 02:48:23 -0500

    Fix router basename in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -98,7 +98,7 @@ const router = createBrowserRouter([
       { path: '*', element: <NoMatch /> }
     ]
   }
-])
+], { basename: '/Movinin-Plus/' })
 
 const App = () => <RouterProvider router={router} />
 
